9b218f82215766efa734b72114a7a81ffaa26266
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-04-26 00:07:32 +0530

    Add files via upload

---
+++ Codeforces/Elephant.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ 
+ int main(){
+    int x;
+    cin>>x;
+    int steps = (x+4)/5;
+
+    cout<<steps<<"\n";
+    
+    return 0;
+    }
+    
+
+
+
+
+    
