472cbf208ce271d7d9bb9ee1cd76639c783537aa
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-04-30 20:55:52 +0530

    Add files via upload

---
+++ String basics/AntonandDanik.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int anton=0;
+    int danik=0;
+    for(int i=0;i<s.length();++i){
+        if(s[i]=='A'){
+            anton++;
+        }else danik++;
+    }
+    if(anton>danik){
+        cout<<"Anton"<<endl;
+    }
+    else if(danik>anton){
+        cout<<"Danik"<<endl;
+    }    
+    else cout<<"Friendship"<<endl;
+
+    return 0;
+}
