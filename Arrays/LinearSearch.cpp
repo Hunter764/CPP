29288e9af9a949161e9e69ee5ddb0daba6b3c58e
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-05-08 19:59:53 +0530

    Update LinearSearch.cpp

--- Arrays/LinearSearch.cpp
+++ Arrays/LinearSearch.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>n;
     int arr[n];
     //input
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<=n-1;i++){
         cin>>arr[i];
     }
     int x;
@@ -15,7 +15,7 @@ int main(){
     //search 
     //search mark
     bool flag = false;  //false not present
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<=n-1;i++){
         if(arr[i]==x) flag=true;
     }
     if(flag==true) cout<<"element found";
