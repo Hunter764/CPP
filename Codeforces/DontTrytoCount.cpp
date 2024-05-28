bd2cf711426b191a692e07bb443a252f81b00df6
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-05-28 23:27:05 +0530

    Add files via upload

---
+++ Codeforces/DontTrytoCount.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n >> m;
+
+        string x , s;
+        getline(cin,x);
+        getline(cin,s);
+
+        int n = x.length();
+        int m = s.length();
+
+        
+        
+    }
+}
