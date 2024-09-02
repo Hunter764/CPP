4dce1deee79e1d4fc24bc9b73c13571d0bc33a3a
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-09-02 23:36:56 +0530

    Add files via upload

---
+++ Competitive/Basics/Basics.cpp
@@ -0,0 +1,95 @@
+/*
+->Basic structure and header file
+->Understanding variables 
+->cout
+->Datatypes
+->Simple operators
+->modulo operator
+->increementation decrementation
+->Realational operators
+->characters and ASCII
+->Understading cin
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // cout<<"Hello";
+    // cout<<sqrt(16);
+
+    //variables->to store data
+    // int a=2;
+    // int b=3;
+    //here a and b are int datatype
+
+
+    //   -> datatypes
+
+    //   ->char,int , float , double,bool
+    //   ->long int, long long int    
+
+    // char c='a';
+    // int a = 3;
+    // double b = 3.5;
+    // a = 4.5;
+    // bool d =123;
+    // cout<<c<<" "<<a<<" "<<b<<" "<<d<<endl;
+
+    //output:- a 4 3.5 1
+
+
+    /* Note:- in programming languages everything number except 0 is true(1) */
+
+    //  ->modulo operator
+
+    //cout<< 30%7<<endl; //2
+
+
+    //  ->incrementation
+    /*
+    pre incrementation :- increment then use
+    post incrementation :- use tehn increment 
+    */
+    // int a = 5;
+    // a = a+1;
+    // cout<<a<<endl;   //6
+    // cout<<a++<<endl; //6
+    // cout<<a<<endl; //7
+    // cout<<++a<<endl; //8
+
+    // ->characters and ASCII
+
+    // char c = 'a';
+    // char b = 'd';
+    // cout<< (int) c<<endl; //type casting //97
+    // cout<< c-b; //-3
+
+
+    // ->input
+
+    
+    // char c ;
+    // cin>>c; //9
+    // cout<< (int) c; //57
+
+    /*Note:- in the above 9 is not a number its a character whoe ascii value is 57 and which is been printed*/
+
+    // endl or "\n" for new line
+
+
+    // ->imp
+    // int a,b,c;
+    // char  d;
+    // cin>> d>>a; //12 12312
+    // cout<<d<<" "<<a ; //1 2
+
+
+
+
+
+
+
+
+
+}
