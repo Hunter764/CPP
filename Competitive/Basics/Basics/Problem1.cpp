a143c9e281f3cb3f3d0518240432a2afffe9a415
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-09-02 23:38:12 +0530

    Add files via upload

---
+++ Competitive/Basics/Basics/Problem1.cpp
@@ -0,0 +1,34 @@
+/*
+Given rectangle of length l and b. Print area of rectangle 
+
+Constraints:
+1 <=l <=10^9
+1 <=b <= 10^9
+
+Input format:
+2 space sepersted integer l and b
+
+Output Format
+Single number whoch is area of rectangle 
+
+Sample input :
+3 4
+
+Sample output:
+12
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int l,b;
+    cin>>l>>b;
+
+    cout<< l * 1LL * b<<endl;
+}
+
+// 12312231
+// 12312312
+// 151592029488072
