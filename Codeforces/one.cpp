d55b949bd8f739de1fe7b1af0db731b34f85e2fe
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-09-03 21:34:08 +0530

    Add files via upload

---
+++ Codeforces/one.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int res = (a-a)+(b-a);
+        cout<<res<<endl;
+    }
+    return 0;
+}
