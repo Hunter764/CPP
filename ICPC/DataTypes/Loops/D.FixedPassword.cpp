6eb9abe0d7162d97654c98e7afe416b2cfb1e361
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-10-06 23:29:13 +0530

    Add files via upload

---
+++ ICPC/DataTypes/Loops/D.FixedPassword.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string x;
+    while(true){
+        cin>>x;
+        if(x=="1999"){
+            cout<<"Correct"<<endl;
+            break;
+        }
+        else cout<<"Wrong"<<endl;
+    }
+    return 0;
+}
