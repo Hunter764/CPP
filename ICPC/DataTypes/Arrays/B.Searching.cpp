a348892409057a788dd1ca3f0b3eecb8264ff724
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-10-13 23:06:01 +0530

    Add files via upload

---
+++ ICPC/DataTypes/Arrays/B.Searching.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, X;
+    cin >> N; 
+    vector<int> A(N);    
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i];
+    }
+    
+    cin >> X;
+    int position = -1;
+    
+    for (int i = 0; i < N; ++i) {
+        if (A[i] == X) {
+            position = i;
+            break;  
+        }
+    }    
+    cout << position << endl;
+    
+    return 0;
+}
