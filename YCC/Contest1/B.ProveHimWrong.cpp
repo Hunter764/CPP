8f47e3552f6eb559bdbbafa50b91091de6947016
Abhyudaya <142680229+Hunter764@users.noreply.github.com>
2024-12-24 00:55:44 +0530

    B.ProveHimWrong.cpp

--- YCC/Contest1/B.ProveHimWrong.cpp
+++ YCC/Contest1/B.ProveHimWrong.cpp
@@ -1,3 +1,27 @@
+// /298103033
+
 #include<bits/stdc++.h>
 using namespace std;
 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(pow(3,n-1)>1e9){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+            vector<long long > res;
+            for(int i=0; i<n; i++){
+                res.push_back(pow(3,i));
+            }
+            for(size_t i=0; i<res.size(); i++){
+                cout<<res[i]<<" ";
+            }
+        }
+    }
+    return 0;
+}
